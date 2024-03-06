a658b9ca049d214c1ae46d0048d13d79846951bf
Isabelle Wang <ilwang@smith.edu>
2024-03-06 11:58:44 -0500

    git init

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Calls from the command line:");
+    System.out.println("    java Postfix <postfix-expr>");
+    System.out.println("    java Calculate <infix-expr>");
+  }
+}
---
+++ Postfix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.StringReader;
+import java.util.ArrayDeque;
+
+/** 
+ * Class to interpret and compute the result of arithmetic expressions 
+ * in POSTFIX format - 
+ */
+public class Postfix {
+  ArrayDeque<Object> stack; 
+  ArrayDeque<Object> queue; 
+
+
+  /** Run short test */
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+      // If no arguments passed, print instructions
+      System.err.println("Usage:  java Postfix <expr>");
+    } else {
+      // Otherwise, echo what was read in for now
+      Scanner input = new Scanner(new StringReader(args[0]));
+      while (input.hasNext()) {
+        System.out.println(input.next());
+      }
+    }
+  }
+
+}
