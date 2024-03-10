0c36269fb468e5df95dd5fdf4df6ffb5130356c9
Isabelle Wang <ilwang@smith.edu>
2024-03-10 18:44:05 -0400

    Not returning answer in Postfix

--- Postfix.java
+++ Postfix.java
@@ -16,33 +16,39 @@ public class Postfix {
 
     for (int i = 0; i < length; i++) {
       System.out.println("Start: " + stack);
-      //System.out.println(queue);
+      // System.out.println(queue);
       Object queueValue = queue.getFirst();
+      // System.out.println(queue.getFirst());
 
       if (queueValue instanceof Double) {
         stack.push((Double) queueValue);
         queue.removeFirst();
+        System.out.println("Add to stack: " + stack);
+        System.out.println("Remove from queue" + queue);
       } else if (queueValue instanceof Character) {
         Double num1 = stack.pop();
         Double num2 = stack.pop();
-        System.out.println(num1);
-        System.out.println(num2);
+        // System.out.println(num1);
+        // System.out.println(num2);
 
         if ((Character) queueValue == ('+')) {
-          System.out.println("add");
+          // System.out.println("add");
           stack.push(num1 + num2);
         } else if ((Character) queueValue == '-') {
-          System.out.println("sub");
+          // System.out.println("sub");
           stack.push(num1 - num2);
         } else if ((Character) queueValue == '*') {
-          System.out.println("mult");
+          // System.out.println("mult");
           stack.push(num1 * num2);
         } else if ((Character) queueValue == '/') {
-          System.out.println("div");
+          // System.out.println("div");
           stack.push(num2 / num1);
         }
+        queue.removeFirst();
       }
     }
+
+    System.out.println(stack.getFirst());
     return stack.getFirst();
 
   }
@@ -52,15 +58,15 @@ public class Postfix {
 
     compute(" 3 2 + 5 *");
     // if (args.length == 0) {
-    //   // If no arguments passed, print instructions
-    //   System.err.println("Usage: java Postfix <expr>");
+    // // If no arguments passed, print instructions
+    // System.err.println("Usage: java Postfix <expr>");
     // } else {
-    //   // Otherwise, echo what was read in for now
-    //   Scanner input = new Scanner(new StringReader(args[0]));
-    //   while (input.hasNext()) {
-    //     compute(args[0]);
-    //     // System.out.println(input.next());
-    //   }
+    // // Otherwise, echo what was read in for now
+    // Scanner input = new Scanner(new StringReader(args[0]));
+    // while (input.hasNext()) {
+    // compute(args[0]);
+    // // System.out.println(input.next());
+    // }
 
     // }
   }
